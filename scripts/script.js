4dfe00457df40fcab7093298972b9572fd95beb8
tranqn <namquoctran24@gmail.com>
2025-09-11 19:20:14 +0200

    added bg.png embedded bg.png feat saveBooks()

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ function loadBookCards(){
     for(let bookIndex = 0; bookIndex < books.length; bookIndex++){
             galleryRef.innerHTML += createBookCard(bookIndex);
             createComments(bookIndex);
+            loadHearts(bookIndex);
     }
 }
 
@@ -17,6 +18,7 @@ function addComment(bookIndex){
     books[bookIndex].comments.unshift(comment);
     createComments(bookIndex);
     commentRef.value = "";
+    saveBooks();
 }
 
 function addLike(bookIndex){
@@ -24,17 +26,31 @@ function addLike(bookIndex){
     const likesRef = document.getElementById(`likes${bookIndex}`);
 
     if(likeIconRef.classList.contains("make-red")){
+
         likeIconRef.src="./assets/icons/heart-thin-icon.png";
         likeIconRef.classList.remove("make-red");
         books[bookIndex].likes = books[bookIndex].likes - 1;
         likesRef.innerHTML = books[bookIndex].likes;
-
+        saveBooks();
     }
     else{
         likeIconRef.src="./assets/icons/heart-icon.png";
         likeIconRef.classList.add("make-red");
         books[bookIndex].likes = books[bookIndex].likes + 1;
         likesRef.innerHTML = books[bookIndex].likes;
-        
+        saveBooks();
+    }
+}
+
+function saveBooks(){
+    localStorage.setItem("books", JSON.stringify(books));
+}
+
+function loadHearts(bookIndex){
+    const likedRef = document.getElementById(`like-icon${bookIndex}`);
+
+    if(books[bookIndex].liked == true){
+        likedRef.src="./assets/icons/heart-icon.png";
+        likedRef.classList.add("make-red");
     }
 }
